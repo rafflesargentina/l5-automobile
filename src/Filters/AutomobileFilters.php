864c269c63e3c54fe05058b0f0b2efe2b8e14915
Mario Patronelli <mario@raffles.com.ar>
2017-08-25 20:30:51 -0300

    Fixes method returns. Add more filters.

--- src/Filters/AutomobileFilters.php
+++ src/Filters/AutomobileFilters.php
@@ -6,18 +6,38 @@ use RafflesArgentina\FilterableSortable\QueryFilters;
 
 class AutomobileFilters extends QueryFilters
 {
+    public function term($query)
+    {
+        return $this->builder->where(\DB::raw('CONCAT(brand, " ",type, " ",model)'), 'LIKE', '%'.$query.'%');
+    }
+
     public function type($query)
     {
-        return $this->query->where('type', $query);
+        return $this->builder->where('type', $query);
     }
 
     public function brand($query)
     {
-        return $this->query->where('brand', $query);
+        return $this->builder->where('brand', $query);
     }
 
     public function model($query)
     {
-        return $this->query->where('model', $query);
+        return $this->builder->where('model', $query);
+    }
+
+    public function type_id($query)
+    {
+        return $this->builder->where('type_id', $query);
+    }
+
+    public function brand_id($query)
+    {
+        return $this->builder->where('brand_id', $query);
+    }
+
+    public function model_id($query)
+    {
+        return $this->builder->where('model_id', $query);
     }
 }

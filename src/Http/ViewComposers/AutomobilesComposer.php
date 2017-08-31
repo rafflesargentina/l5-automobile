b17f2c76bbe8bdfa87ec44f136efcc4f824f02d3
Mario Patronelli <mario@raffles.com.ar>
2017-08-31 16:36:13 -0300

    Update AutomobileComposer.php

--- src/Http/ViewComposers/AutomobilesComposer.php
+++ src/Http/ViewComposers/AutomobilesComposer.php
@@ -4,20 +4,25 @@ namespace RafflesArgentina\Automobile\Http\ViewComposers;
 
 use Illuminate\Contracts\View\View;
 
+use RafflesArgentina\Automobile\Sorters\AutomobileSorters;
 use RafflesArgentina\Automobile\Repositories\AutomobileRepository;
 
 class AutomobilesComposer
 {
-    protected $automobile;
+    protected $sorters, $automobile;
 
-    public function __construct(AutomobileRepository $automobile)
+    public function __construct(AutomobileSorters $sorters, AutomobileRepository $automobile)
     {
+        $this->sorters = $sorters;
         $this->Automobile = $automobile;
     }
 
     public function composeIndex(View $view)
     {
-        //
+        $view->with('order', $this->sorters->order())
+             ->with('orderBy', $this->sorters->orderBy())
+             ->with('sources', $this->Automobile->pluckSources())
+             ->with('orderByKeys', AutomobileSorters::listOrderByKeys());
     }
 
     public function composeCreate(View $view)
@@ -32,8 +37,9 @@ class AutomobilesComposer
 
     public function composeEdit(View $view)
     {
-        $view->with('brands', $this->Automobile->findBy('id', request()->route('automobile'))->pluck('brand', 'brand'))
+        $view->with('years', $this->Automobile->pluckYears())
              ->with('types', $this->Automobile->findBy('id', request()->route('automobile'))->pluck('type', 'type'))
+             ->with('brands', $this->Automobile->findBy('id', request()->route('automobile'))->pluck('brand', 'brand'))
              ->with('sources', $this->Automobile->pluckSources());
     }
 }

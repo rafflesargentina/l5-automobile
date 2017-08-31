f7c39b759ae820b30d99eda52e60f45ec3396701
Mario Patronelli <mario@raffles.com.ar>
2017-08-31 19:54:38 -0300

    Change ViewComposerServiceProvider.php to load automobiles view locations form config resource_name variable first or fallback to 'automobiles'

--- src/Providers/ViewComposerServiceProvider.php
+++ src/Providers/ViewComposerServiceProvider.php
@@ -14,16 +14,17 @@ class ViewComposerServiceProvider extends ServiceProvider
     public function boot()
     {
         $module = config('automobile.module') ?: 'automobile';
+        $resourceName = config('automobile.resource_name') ?: 'automobiles';
         view()->composer($module.'::partials.header', 'RafflesArgentina\Automobile\Http\ViewComposers\LayoutPartialsComposer@composeHeader');
         view()->composer('vendor.automobile.partials.header', 'RafflesArgentina\Automobile\Http\ViewComposers\LayoutPartialsComposer@composeHeader');
 
-        view()->composer($module.'::automobiles.index', 'RafflesArgentina\Automobile\Http\ViewComposers\AutomobilesComposer@composeIndex');
+        view()->composer($module.'::'.$resourceName.'.index', 'RafflesArgentina\Automobile\Http\ViewComposers\AutomobilesComposer@composeIndex');
         view()->composer('vendor.automobile.index', 'RafflesArgentina\Automobile\Http\ViewComposers\AutomobilesComposer@composeIndex');
 
-        view()->composer($module.'::automobiles.partials.pluma-create-form', 'RafflesArgentina\Automobile\Http\ViewComposers\AutomobilesComposer@composeCreate');
+        view()->composer($module.'::'.$resourceName.'.partials.pluma-create-form', 'RafflesArgentina\Automobile\Http\ViewComposers\AutomobilesComposer@composeCreate');
         view()->composer('vendor.automobile.automobiles.partials.pluma-create-form', 'RafflesArgentina\Automobile\Http\ViewComposers\AutomobilesComposer@composeCreate');
 
-        view()->composer($module.'::automobiles.partials.pluma-edit-form', 'RafflesArgentina\Automobile\Http\ViewComposers\AutomobilesComposer@composeEdit');
+        view()->composer($module.'::'.$resourceName.'.partials.pluma-edit-form', 'RafflesArgentina\Automobile\Http\ViewComposers\AutomobilesComposer@composeEdit');
         view()->composer('vendor.automobile.automobiles.partials.pluma-edite-form', 'RafflesArgentina\Automobile\Http\ViewComposers\AutomobilesComposer@composeEdit');
     }
 }

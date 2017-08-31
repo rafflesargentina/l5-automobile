6104e2d6bbed850ad57db60ea6ecf8bb1d5f6e9d
Mario Patronelli <mario@raffles.com.ar>
2017-08-31 20:13:15 -0300

    Show filters and sorters only if there's any

--- src/Resources/Views/partials/applied-filters-and-sorters.blade.php
+++ src/Resources/Views/partials/applied-filters-and-sorters.blade.php
@@ -1,3 +1,4 @@
+@if($appliedFilters || $appliedSorters)
 <ul class="Lista-Inline">
   <li><span class="Texto-Secundario">Ordenados por: </span>
   @foreach($appliedSorters as $k => $v)
@@ -10,3 +11,4 @@
   @endforeach
   </li>
 </ul>
+@endif

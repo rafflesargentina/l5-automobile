0cc1d8c017cc2d4a3d509138f53543ee9eaf5944
Mario Patronelli <mario@raffles.com.ar>
2017-09-07 19:57:56 -0300

    Add route alias handling for Routes/web.php

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -1,8 +1,14 @@
 <?php
 
-Route::resource((config('automobile.resource_name') ?: 'automobiles'), 'AutomobilesController');
-Route::resource((config('automobile.type_resource_name') ?: 'automobile-types'), 'AutomobileTypesController', ['only' => ['index']]);
-Route::resource((config('automobile.brand_resource_name') ?: 'automobile-brands'), 'AutomobileBrandsController', ['only' => ['index']]);
-Route::resource((config('automobile.model_resource_name') ?: 'automobile-models'), 'AutomobileModelsController', ['only' => ['index']]);
+if (config('automobile.alias')) {
+    $alias .= '.';
+} else {
+    $alias = null;
+}
 
-Route::get((config('automobile.resource_name') ?: 'automobiles').'/dropdown/{groupBy}', 'S2DropdownsController');
+Route::resource($alias.(config('automobile.resource_name') ?: 'automobiles'), 'AutomobilesController');
+Route::resource($alias.(config('automobile.type_resource_name') ?: 'automobile-types'), 'AutomobileTypesController', ['only' => ['index']]);
+Route::resource($alias.(config('automobile.brand_resource_name') ?: 'automobile-brands'), 'AutomobileBrandsController', ['only' => ['index']]);
+Route::resource($alias.(config('automobile.model_resource_name') ?: 'automobile-models'), 'AutomobileModelsController', ['only' => ['index']]);
+
+Route::get($alias.(config('automobile.resource_name') ?: 'automobiles').'/dropdown/{groupBy}', 'S2DropdownsController');

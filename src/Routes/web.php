960f3d8f7d450d7cd5b24901fa4f3acb47f51ad2
Mario Patronelli <mario@raffles.com.ar>
2017-09-11 09:18:04 -0300

    Removes spaces

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -4,12 +4,10 @@ $alias = config('automobile.alias');
 $prefix = config('automobile.prefix');
 
 Route::prefix($prefix)->group(function() use($alias) {
-
     Route::resource((config('automobile.resource_name') ?: 'automobiles'), 'AutomobilesController', ['as' => $alias]);
     Route::resource((config('automobile.type_resource_name') ?: 'automobile-types'), 'AutomobileTypesController', ['only' => ['index'], 'as' => $alias]);
     Route::resource((config('automobile.brand_resource_name') ?: 'automobile-brands'), 'AutomobileBrandsController', ['only' => ['index'], 'as' => $alias]);
     Route::resource((config('automobile.model_resource_name') ?: 'automobile-models'), 'AutomobileModelsController', ['only' => ['index'], 'as' => $alias]);
 
     Route::get((config('automobile.resource_name') ?: 'automobiles').'/dropdown/{groupBy}', 'S2DropdownsController', ['as' => $alias]);
-
 });
